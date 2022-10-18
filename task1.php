f92cb6df24bf224c6deb489e3886ad4a080228dd
Vadim Varnavskiy <varnavskiy3579mode@mail.ru>
2022-10-19 00:32:29 +0300

    Документы отформатированы и прошли валидацию

--- task1.php
+++ task1.php
@@ -1,19 +1,15 @@
 <?php
-    function calculator(string $a): string
-    {
+function calculator(string $a): string
+{
     $calculator = ($a);
-    return eval ("return $a;");
-    }
-    echo calculator("1+1"), "<br>";
-    echo calculator("1+1-2"), "<br>";
-    echo calculator("4+1-2+3"), "<br>";
-    echo calculator("4*1*2"), "<br>";
-    if ($a = "str") 
-    {
+    return eval("return $a;");
+}
+echo calculator("1+1"), "<br>";
+echo calculator("1+1-2"), "<br>";
+echo calculator("4+1-2+3"), "<br>";
+echo calculator("4*1*2"), "<br>";
+if ($a = "str") {
     echo "Incorrect input";
-    }
-    else 
-    {
+} else {
     echo "str";
-    }
-?>
+}
