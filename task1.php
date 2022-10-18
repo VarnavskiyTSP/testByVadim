7d4001f183b7f27764cefecdf025492cf3b93c87
Vadim Varnavskiy <varnavskiy3579mode@mail.ru>
2022-10-18 23:24:24 +0300

    Перевыполнил task1.php

--- task1.php
+++ task1.php
@@ -1,20 +1,19 @@
 <?php
-    $number = '1';
-    $numbers = explode('+', $number, 5);
-    echo $number + $number, "<br>";
-	
-    $number2 = '2';
-    $numbers = explode('-', $number2, 5);
-    echo $number + $number - $number2, "<br>";
-
-    $number3 = '3';
-    $numbers = explode('+', $number3, 5);
-    $number4 = '4';
-    $numbers = explode('+', $number4, 5);
-    echo $number4 + $number - $number2 + $number3, "<br>";
-
-    echo $number4 * $number * $number2, "<br>";
-
-    $number5 = 'str';
-    echo $number5, "<br>";
+    function calculator(string $a): string
+    {
+    $calculator = ($a);
+    return eval ("return $a;");
+    }
+    echo calculator("1+1"), "<br>";
+    echo calculator("1+1-2"), "<br>";
+    echo calculator("4+1-2+3"), "<br>";
+    echo calculator("4*1*2"), "<br>";
+    if ($a = "str") 
+    {
+    echo "Incorrect input";
+    }
+    else 
+    {
+    echo "str";
+    }
 ?>
