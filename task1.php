541b7fc8f89e6b5723cf9be936722db646dc424d
Vadim Varnavskiy <varnavskiy3579mode@mail.ru>
2022-10-23 00:27:37 +0300

    исправлены пробелы

--- task1.php
+++ task1.php
@@ -1,24 +1,24 @@
 <?php
 function calculator(string $a): string
 {
-	$checklistSymbols = ['+', '-', '*', '/'];
-	$checklistNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-	$result = '';
+    $checklistSymbols = ['+', '-', '*', '/'];
+    $checklistNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    $result = '';
 
-	if (strpos($a, '/0')) {
-		return 'Incorrect input';
-	} else {
-		$numericalExpressionArray = str_split($a);
-	}
+    if (strpos($a, '/0')) {
+        return 'Incorrect input';
+    } else {
+        $numericalExpressionArray = str_split($a);
+    }
 
-	foreach ($numericalExpressionArray as $keyArray) {
-		if (!(in_array($keyArray, $checklistSymbols) && in_array($keyArray, $checklistNumbers))) {
-			$result .= $keyArray;
-		} else {
-			return 'Incorrect input';
-		}
-	}
-	return eval('return ' . $result . ';');
+    foreach ($numericalExpressionArray as $keyArray) {
+        if (!(in_array($keyArray, $checklistSymbols) && in_array($keyArray, $checklistNumbers))) {
+            $result .= $keyArray;
+        } else {
+            return 'Incorrect input';
+        }
+    }
+    return eval('return ' . $result . ';');
 }
 
 
