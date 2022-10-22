ce767c77681e12dff44b8b784b91611bed6521a4
Vadim Varnavskiy <varnavskiy3579mode@mail.ru>
2022-10-22 23:50:32 +0300

    исправлены все задания и проверка на валидацию

--- task2.php
+++ task2.php
@@ -1,7 +1,9 @@
 <?php
 function sum(string $one, string $two): string
 {
-    $sum = strtotime($one) + strtotime($two) - strtotime('00:00:00');
-    return date('H:i:s', $sum);
+    if (!strtotime($one) || !strtotime($two)) {
+        return 'Incorrect input';
+    }
+    return date('H:i:s', strtotime($one) + strtotime($two));
 }
 echo sum('10:20:30', '10:20:30');
